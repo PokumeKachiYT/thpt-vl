bfdc924ee05c387939aabe07930138ebb68de561
PokumeKachiYT <pkmcgaming@gmail.com>
2024-05-21 13:50:53 +0700

    addeed stuff

--- public/home/src.js
+++ public/home/src.js
@@ -1,1 +1,21 @@
-console.log("hehe")
+let ticking = false;
+
+function getScrollPercentage( elm ){
+  var p = document.body.parentNode
+  return (document.body.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight ) * 100
+}
+
+const onScroll = (scrollY) => {
+  console.log(getScrollPercentage())
+}
+
+document.addEventListener('scroll', (event) => {
+  if (!ticking) {
+    ticking = true;
+
+    window.requestAnimationFrame(() => {
+      onScroll(window.scrollY)
+      ticking = false;
+    })
+  }
+})
