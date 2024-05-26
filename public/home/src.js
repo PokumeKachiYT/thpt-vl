9cf5fa724190a6366dec7c310a737cc55fe1060f
PokumeKachiYT <pkmcgaming@gmail.com>
2024-05-26 14:16:46 +0700

    added stuff

--- public/home/src.js
+++ public/home/src.js
@@ -1,20 +1,69 @@
 let ticking = false;
 
+const settings = [
+  {
+    'id': 'header',
+    'start': -15,
+    'stop': 20,
+  },
+  {
+    'id': 'intro',
+    'start': 25,
+    'stop': 55,
+  },
+  {
+    'id': 'body1',
+    'start': 45,
+    'stop': 75,
+  },
+  {
+    'id': 'donation',
+    'start': 70,
+    'stop': 90,
+  },
+  {
+    'id': 'outro',
+    'start': 85,
+    'stop': 115,
+  },
+  {
+    'id': 'signature',
+    'start': 90,
+    'stop': 110,
+  },
+];
+
 function getScrollPercentage( elm ){
-  var p = document.body.parentNode
-  return (document.body.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight ) * 100
+  var p = document.body.parentNode;
+  return (document.body.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight ) * 100;
 }
 
-const onScroll = (scrollY) => {
-  console.log(getScrollPercentage())
+const onScroll = () => {
+  const scrollPercentage = getScrollPercentage();
+
+  for (const info of settings) {
+    const element = document.getElementById(info.id);
+
+    if (element != null) {  
+      if (scrollPercentage < info.start || scrollPercentage > info.stop) {
+        element.style.opacity = 0;
+      } else {
+        const mid = (info.stop + info.start) / 2;
+
+        element.style.opacity = 1 - Math.abs(mid - scrollPercentage) / (info.stop - info.start) * 2;
+      }
+    }
+  }
 }
+
+onScroll()
 
 document.addEventListener('scroll', (event) => {
   if (!ticking) {
     ticking = true;
 
     window.requestAnimationFrame(() => {
-      onScroll(window.scrollY)
+      onScroll();
       ticking = false;
     })
   }
